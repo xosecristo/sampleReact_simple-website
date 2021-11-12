6be18751dd69e810182854e972f9f5b5310c51fe
xosecristo <xosecristo@outlook.com>
2021-11-12 11:32:03 -0700

    hotfix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import cardimg from '../media/card-preview.jpg'
 
 export const Card = (props) => {
     return (
-        <div className='card'>
+        <div className='card' alt='card preview'>
             <img src={cardimg} />
             <h2>{props.title}</h2>
             <p>{props.desc}</p>
