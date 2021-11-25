7b97a08168ee86defb18c73c1e4b2d5c1dd36b61
xosecristo <xosecristo@outlook.com>
2021-11-25 03:53:09 -0700

    hotfixes and details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import placeholderimg from '../media/card-preview.jpg'
 export const Card = (props) => {
     return (
         <div className='card'>
-            <img src={placeholderimg} alt='image preview' />
+            <img src={placeholderimg} alt='card preview' />
             <h2>{props.title}</h2>
             <p>{props.desc}</p>
             <button>{props.cta}</button>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { Hero } from '../components/Hero'
+// import { Hero } from '../components/Hero'
 import placeholderimg from '../media/card-preview.jpg'
 
 export const About = () => {
     return (
         <section id="about">
-            <Hero
-                title="About Us"
-                text="Lorem ipsum dolor amet consectetur adipisicing elit. Reprehenderit tenetur eligendi modi? sit amet consectetur adipisicing elit. Reprehenderit tenetur eligendi modi?"
-                path="/"
-                cta="Call to action" />
-            <img src={placeholderimg} alt="about us placeholder image" />
+            <div>
+                <h1>About us</h1>
+                <p>Lorem ipsum dolor amet consectetur adipisicing elit. Reprehenderit tenetur eligendi modi? sit amet consectetur adipisicing elit. Reprehenderit tenetur eligendi modi?</p>
+            </div>
+            <img src={placeholderimg} alt="about us placeholder" />
 
         </section>
     )
